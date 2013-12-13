a91c7bacb576e428e8a69e3c71434f98a580d9bf
Sven Schultschik <sven@schultschik.de>
2013-12-13 06:40:12 +0100

    change from iframe to open videos in colorbox

--- components/com_schuweb_gallery/views/videos/tmpl/default_videos.php
+++ components/com_schuweb_gallery/views/videos/tmpl/default_videos.php
@@ -14,16 +14,15 @@ defined('_JEXEC') or die;
     <?php foreach ($this->videos as $video) : ?>
         <li class="span<?php echo $this->video_grid_size; ?>">
             <?php if ($video->video_service == 'SCHUWEB_GALLERY_VIMEO') : ?>
-                <iframe src="http://player.vimeo.com/video/<?php echo $video->video_id ?>"
-                        width="<?php echo $this->video_width; ?>" height="<?php echo $this->video_height; ?>"
-                        frameborder="0"
-                        webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+                <a href="http://player.vimeo.com/video/<?php echo $video->video_id ?>" class="vimeo groupVideos">
+                    <img src="<?php //echo $image['thumb']; ?>" alt="<?php echo $video->name; ?>">
+                </a>
             <?php endif; ?>
             <?php if ($video->video_service == 'SCHUWEB_GALLERY_YOUTUBE') : ?>
-                <iframe id="player" type="text/html" width="<?php echo $this->video_width; ?>"
-                        height="<?php echo $this->video_height; ?>"
-                        src="http://www.youtube.com/embed/<?php echo $video->video_id ?>?enablejsapi=1"
-                        frameborder="0"></iframe>
+                <a href="http://www.youtube.com/embed/<?php echo $video->video_id ?>?rel=0&amp;wmode=transparent&amp;enablejsapi=1"
+                   class="youtube groupVideos">
+                    <img src="<?php //echo $image['thumb']; ?>" alt="<?php echo $video->name; ?>">
+                </a>
             <?php endif; ?>
 
             <?php if ($this->displayVideoName == 1) : ?>
--- media/js/schuweb_colorbox.js
+++ media/js/schuweb_colorbox.js
@@ -1,3 +1,5 @@
 jQuery(document).ready(function () {
     jQuery('a.group_images').colorbox({rel:'group_images', maxWidth: '100%', maxHeight: '100%'});
+    jQuery("a.youtube").colorbox({iframe:true, rel:'groupVideos', innerWidth:640, innerHeight:390, maxWidth: '100%', maxHeight: '100%'});
+    jQuery("a.vimeo").colorbox({iframe:true, rel:'groupVideos', innerWidth:500, innerHeight:409, maxWidth: '100%', maxHeight: '100%'});
 });
